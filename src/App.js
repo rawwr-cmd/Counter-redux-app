ba545d90a0950149cceab627406b4f6c471270f6
rawwr <akshayace0302@gmail.com>
2022-07-01 18:05:04 +0530

    working with multiple state properties

--- src/App.js
+++ src/App.js
@@ -3,8 +3,8 @@ import ClassCounter from "./components/classBasedCounter";
 
 function App() {
   return (
-    // <Counter />
-    <ClassCounter />
+    <Counter />
+    // <ClassCounter />
   );
 }
 
--- src/store/index.js
+++ src/store/index.js
@@ -1,16 +1,35 @@
 import { createStore } from "redux";
 
 //reducer
-const counteReducer = (state = { counter: 0 }, action) => {
+
+const initialState = { counter: 0, showCounter: true };
+
+const counteReducer = (state = initialState, action) => {
   switch (action.type) {
     case "INCREMENT":
       return {
         counter: state.counter + 1,
+        showCounter: state.showCounter,
       };
+
+    case "INCREASE":
+      return {
+        counter: state.counter + action.amount,
+        showCounter: state.showCounter,
+      };
+
     case "DECREMENT":
       return {
         counter: state.counter - 1,
+        showCounter: state.showCounter,
       };
+
+    case "TOGGLE_COUNTER":
+      return {
+        showCounter: !state.showCounter,
+        counter: state.counter,
+      };
+
     default:
       return state;
   }
